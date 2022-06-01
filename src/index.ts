57d73c455da6824db03cb8dba7f28d6919114e46
jianyun2020 <sujianyun2020@163.com>
2022-06-01 08:58:47 +0800

    链表实现队列

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@
 // import './01-algorithm/01-array-rotate'
 // import './01-algorithm/02-bracket-matching'
 // import './01-algorithm/03-two-stacks-one-queue'
-import './01-algorithm/04-reverse-link-list'
+// import './01-algorithm/04-reverse-link-list'
+import './01-algorithm/04-queue-with-list'
